cd0e9d5366574e7c80188eeba29670f6dd6c97c2
Alex :) <alexandre.cavet@colombbus.org>
2025-05-21 23:00:15 +0200

    Update UserCrudController.php

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -6,10 +6,12 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -31,9 +33,20 @@ class UserCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id')->setDisabled()->setFormTypeOption('disabled', true),
+            IdField::new('id')->setDisabled()->setFormTypeOption('disabled', true)->hideOnForm(),
             EmailField::new('email')->onlyOnIndex()->setLabel('E-mail'),
             TextField::new('username')->setLabel('Nom d\'utilisateur'),
+            ChoiceField::new('roles')
+                ->setLabel('Rôles')
+                ->setChoices([
+                    // Customize these roles according to your application
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                    // Add other roles here, e.g., 'Modérateur' => 'ROLE_MODERATOR'
+                ])
+                ->allowMultipleChoices() // Allows selecting multiple roles
+                ->renderExpanded() // Renders as checkboxes
+                ->setHelp('Sélectionnez les rôles de l\'utilisateur.'),
         ];
     }
 }
